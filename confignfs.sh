123c98123c3df52c2daa3562365a1c82fd38b1ed
grandparoach <stevenwroach@hotmail.com>
2016-01-12 21:28:26 -0700

    Create confignfs.sh

---
+++ confignfs.sh
@@ -0,0 +1,43 @@
+# accepts a blob uri (MUST HAVE https:// at the beginning) and a key
+
+[[ -z "$HOME" || ! -d "$HOME" ]] && { echo 'fixing $HOME'; HOME=/root; } 
+export HOME 
+
+# apt-get -y update
+# apt-get -y install python3-pip libssl-dev libffi-dev npm
+yum -y update
+yum -y upgrade
+yum -y install epel-release gcc libffi-devel openssl-devel python-devel
+yum -y install python-pip nodejs npm
+pip install blobxfer --upgrade  
+blobxfer --version  
+
+ln -s /usr/bin/nodejs /usr/bin/node
+npm install -g azure-cli 
+azure config mode arm 
+
+sa_domain=$(echo "$1" | cut -f3 -d/)
+sa_name=$(echo $sa_domain | cut -f1 -d.)
+container_name=$(echo "$1" | cut -f4 -d/)
+blob_name=$(echo "$1" | cut -f5 -d/)
+
+echo "sa name, container name, blob name:"    
+echo $sa_name    
+echo $container_name    
+echo $blob_name    
+echo "$container_name,$blob_name" > /mnt/resource/config.txt  
+
+attempts=0
+response=1
+while [ $response -ne 0 -a $attempts -lt 5 ]
+do
+  blobxfer $sa_name $container_name /mnt/resource/ --remoteresource $blob_name --storageaccountkey $2 --download --no-computefilemd5
+  response=$?
+  attempts=$((attempts+1))
+done
+
+
+
+
+
+
