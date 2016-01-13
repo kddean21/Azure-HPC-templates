7ce38e503dc5b7d7149e0349096f13237d288c0b
grandparoach <stevenwroach@hotmail.com>
2016-01-13 05:41:35 -0700

    Update confignfs.sh

--- confignfs.sh
+++ confignfs.sh
@@ -1,43 +1,189 @@
-# accepts a blob uri (MUST HAVE https:// at the beginning) and a key
+#!/bin/bash
 
-[[ -z "$HOME" || ! -d "$HOME" ]] && { echo 'fixing $HOME'; HOME=/root; } 
-export HOME 
+set -x
+#set -xeuo pipefail
 
-# apt-get -y update
-# apt-get -y install python3-pip libssl-dev libffi-dev npm
-yum -y update
-yum -y upgrade
-yum -y install epel-release gcc libffi-devel openssl-devel python-devel
-yum -y install python-pip nodejs npm
-pip install blobxfer --upgrade  
-blobxfer --version  
+if [[ $(id -u) -ne 0 ]] ; then
+    echo "Must be run as root"
+    exit 1
+fi
 
-ln -s /usr/bin/nodejs /usr/bin/node
-npm install -g azure-cli 
-azure config mode arm 
+if [ $# != 5 ]; then
+    echo "Usage: $0 <MasterHostname> <WorkerHostnamePrefix> <WorkerNodeCount> <HPCUserName> <TemplateBaseUrl>"
+    exit 1
+fi
 
-sa_domain=$(echo "$1" | cut -f3 -d/)
-sa_name=$(echo $sa_domain | cut -f1 -d.)
-container_name=$(echo "$1" | cut -f4 -d/)
-blob_name=$(echo "$1" | cut -f5 -d/)
+# Set user args
+MASTER_HOSTNAME=$1
+WORKER_HOSTNAME_PREFIX=$2
+WORKER_COUNT=$3
+TEMPLATE_BASE_URL="$5"
+LAST_WORKER_INDEX=$(($WORKER_COUNT - 1))
 
-echo "sa name, container name, blob name:"    
-echo $sa_name    
-echo $container_name    
-echo $blob_name    
-echo "$container_name,$blob_name" > /mnt/resource/config.txt  
-
-attempts=0
-response=1
-while [ $response -ne 0 -a $attempts -lt 5 ]
-do
-  blobxfer $sa_name $container_name /mnt/resource/ --remoteresource $blob_name --storageaccountkey $2 --download --no-computefilemd5
-  response=$?
-  attempts=$((attempts+1))
-done
+# Shares
+SHARE_HOME=/share/home
+SHARE_DATA=/share/data
 
 
+# Hpc User
+HPC_USER=$4
+HPC_UID=7007
+HPC_GROUP=hpc
+HPC_GID=7007
+
+
+# Returns 0 if this node is the master node.
+#
+is_master()
+{
+    hostname | grep "$MASTER_HOSTNAME"
+    return $?
+}
+
+
+# Installs all required packages.
+#
+install_pkgs()
+{
+    pkgs="zlib zlib-devel bzip2 bzip2-devel bzip2-libs openssl openssl-devel openssl-libs gcc gcc-c++ nfs-utils rpcbind mdadm wget"
+    yum -y install $pkgs
+}
+
+# Partitions all data disks attached to the VM and creates
+# a RAID-0 volume with them.
+#
+setup_data_disks()
+{
+    mountPoint="$1"
+    createdPartitions=""
+
+    # Loop through and partition disks until not found
+    for disk in sdc sdd sde sdf sdg sdh sdi sdj sdk sdl sdm sdn sdo sdp sdq sdr; do
+        fdisk -l /dev/$disk || break
+        fdisk /dev/$disk << EOF
+n
+p
+1
+
+
+t
+fd
+w
+EOF
+        createdPartitions="$createdPartitions /dev/${disk}1"
+    done
+
+    # Create RAID-0 volume
+    if [ -n "$createdPartitions" ]; then
+        devices=`echo $createdPartitions | wc -w`
+        mdadm --create /dev/md10 --level 0 --raid-devices $devices $createdPartitions
+        mkfs -t ext4 /dev/md10
+        echo "/dev/md10 $mountPoint ext4 defaults,nofail 0 2" >> /etc/fstab
+        mount /dev/md10
+    fi
+}
+
+# Creates and exports two shares on the master nodes:
+#
+# /share/home (for HPC user)
+# /share/data
+#
+# These shares are mounted on all worker nodes.
+#
+setup_shares()
+{
+    mkdir -p $SHARE_HOME
+    mkdir -p $SHARE_DATA
+
+    if is_master; then
+        setup_data_disks $SHARE_DATA
+        echo "$SHARE_HOME    *(rw,async)" >> /etc/exports
+        echo "$SHARE_DATA    *(rw,async)" >> /etc/exports
+
+        systemctl enable rpcbind || echo "Already enabled"
+        systemctl enable nfs-server || echo "Already enabled"
+        systemctl start rpcbind || echo "Already enabled"
+        systemctl start nfs-server || echo "Already enabled"
+    else
+        echo "master:$SHARE_HOME $SHARE_HOME    nfs4    rw,auto,_netdev 0 0" >> /etc/fstab
+        echo "master:$SHARE_DATA $SHARE_DATA    nfs4    rw,auto,_netdev 0 0" >> /etc/fstab
+        mount -a
+        mount | grep "^master:$SHARE_HOME"
+        mount | grep "^master:$SHARE_DATA"
+    fi
+}
 
 
 
+
+
+# Adds a common HPC user to the node and configures public key SSh auth.
+# The HPC user has a shared home directory (NFS share on master) and access
+# to the data share.
+#
+setup_hpc_user()
+{
+    # disable selinux
+    sed -i 's/enforcing/disabled/g' /etc/selinux/config
+    setenforce permissive
+    
+    groupadd -g $HPC_GID $HPC_GROUP
+
+    # Don't require password for HPC user sudo
+    echo "$HPC_USER ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+    
+    # Disable tty requirement for sudo
+    sed -i 's/^Defaults[ ]*requiretty/# Defaults requiretty/g' /etc/sudoers
+
+    if is_master; then
+    
+        useradd -c "HPC User" -g $HPC_GROUP -m -d $SHARE_HOME/$HPC_USER -s /bin/bash -u $HPC_UID $HPC_USER
+
+        mkdir -p $SHARE_HOME/$HPC_USER/.ssh
+        
+        # Configure public key auth for the HPC user
+        ssh-keygen -t rsa -f $SHARE_HOME/$HPC_USER/.ssh/id_rsa -q -P ""
+        cat $SHARE_HOME/$HPC_USER/.ssh/id_rsa.pub > $SHARE_HOME/$HPC_USER/.ssh/authorized_keys
+
+        echo "Host *" > $SHARE_HOME/$HPC_USER/.ssh/config
+        echo "    StrictHostKeyChecking no" >> $SHARE_HOME/$HPC_USER/.ssh/config
+        echo "    UserKnownHostsFile /dev/null" >> $SHARE_HOME/$HPC_USER/.ssh/config
+        echo "    PasswordAuthentication no" >> $SHARE_HOME/$HPC_USER/.ssh/config
+
+        # Fix .ssh folder ownership
+        chown -R $HPC_USER:$HPC_GROUP $SHARE_HOME/$HPC_USER
+
+        # Fix permissions
+        chmod 700 $SHARE_HOME/$HPC_USER/.ssh
+        chmod 644 $SHARE_HOME/$HPC_USER/.ssh/config
+        chmod 644 $SHARE_HOME/$HPC_USER/.ssh/authorized_keys
+        chmod 600 $SHARE_HOME/$HPC_USER/.ssh/id_rsa
+        chmod 644 $SHARE_HOME/$HPC_USER/.ssh/id_rsa.pub
+        
+        # Give hpc user access to data share
+        chown $HPC_USER:$HPC_GROUP $SHARE_DATA
+    else
+        useradd -c "HPC User" -g $HPC_GROUP -d $SHARE_HOME/$HPC_USER -s /bin/bash -u $HPC_UID $HPC_USER
+    fi
+}
+
+# Sets all common environment variables and system parameters.
+#
+setup_env()
+{
+    # Set unlimited mem lock
+    echo "$HPC_USER hard memlock unlimited" >> /etc/security/limits.conf
+    echo "$HPC_USER soft memlock unlimited" >> /etc/security/limits.conf
+
+    # Intel MPI config for IB
+    echo "# IB Config for MPI" > /etc/profile.d/hpc.sh
+    echo "export I_MPI_FABRICS=shm:dapl" >> /etc/profile.d/hpc.sh
+    echo "export I_MPI_DAPL_PROVIDER=ofa-v2-ib0" >> /etc/profile.d/hpc.sh
+    echo "export I_MPI_DYNAMIC_CONNECTION=0" >> /etc/profile.d/hpc.sh
+}
+
+install_pkgs
+setup_shares
+setup_hpc_user
+setup_env
 
